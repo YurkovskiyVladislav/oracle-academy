3adfe9ec09330f1e724e688fcf06ec9b066461e7
YurkovskiyVladislav <vladyurkovskij@yandex.ua>
2016-04-04 22:54:28 +0300

    1

--- Parser/src/main/java/oracle/academy/models/Shop.java
+++ Parser/src/main/java/oracle/academy/models/Shop.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@SuppressWarnings("restriction")
+
 @XmlRootElement
 public class Shop {
 	
